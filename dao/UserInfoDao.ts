d09eae2eb15a8d5efc160311d352f120c9585637
panhe-xue <1158817942@qq.com>
2018-09-24 01:18:41 +0800

    a

--- dao/UserInfoDao.ts
+++ dao/UserInfoDao.ts
@@ -1,9 +1,9 @@
 import ms from "../util/ms";
 import * as mysql from "mysql";
 /**
- * 登陆类
+ * 人员信息类
  */
-class UserInfoDao{
+export class UserInfoDao{
     public type: string;
     public type_content: any;
     public pageBegin: number;
@@ -94,4 +94,52 @@ class UserInfoDao{
     }
 }
 
-export default UserInfoDao;
+/**
+ * 人员删除类
+ */
+export class UserInfoDeleteDao{
+    public id: number;
+    /**
+     * 表名
+     */
+    static TABLE_NAME = "user_info";
+
+    constructor(id: number) {
+        this.id = id;
+    }
+    /**
+     * 检查数据的正确性
+     * @return boolean 是否正确
+     */
+    checkData(): Boolean {
+        let status = true; //默认为正确的数据
+        do {
+            if(isNaN(this.id) || this.id < 1) {
+                status = false
+                break;
+            }
+        } while (false)
+
+        return status
+    }
+    /**
+     * 从数据库获取该用户信息
+     * @return Array 数据
+     */
+    async doDel() {
+        let result: any[] = [];
+
+        let whereSqlString: string = '';
+        let sql = `delete from ${UserInfoDeleteDao.TABLE_NAME} where id = ?`;
+        sql = mysql.format(sql, [this.id]);
+        console.info("get user info from db sql:", sql);
+
+        try {
+            let rows = await ms.mysql["siping_public_security"].execSql(sql);
+            return rows;
+        } catch (error) {
+            console.log(sql , "error: ", error);
+            throw new Error(error);
+        }
+    }
+}
--- router/userInfo/UserInfo.ts
+++ router/userInfo/UserInfo.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 
 import {RetCode, RetMsg} from "../../util/RetStatus";
-import UserInfoDao from "../../dao/UserInfoDao";
+import {UserInfoDao, UserInfoDeleteDao} from "../../dao/UserInfoDao";
 export  const route = express.Router();
 
 route.post('/useradmin/userlist', (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -26,7 +26,7 @@ route.post('/useradmin/userlist', (req: express.Request, res: express.Response, 
             if(!UserInfoInstance.checkData()) {
                 ret = RetCode.ERR_CLIENT_PARAMS_ERR;
                 msg = RetMsg.ERR_CLIENT_PARAMS_ERR;
-                subMsg = "登陆参数出错";
+                subMsg = "参数出错";
                 break;
             }
             console.log("checkData success!!");
@@ -48,6 +48,51 @@ route.post('/useradmin/userlist', (req: express.Request, res: express.Response, 
             msg   : msg,
             subMsg: subMsg,
             data  : dataRows
+        }
+        //返回操作
+        res.json(result);
+        next();
+    })()
+});
+
+route.get('/useradmin/delete', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    (async () => {
+        let dataRows; //数据库用户情况
+        let ret:number = RetCode.SUC_OK;
+        let msg: string = RetMsg.SUC_OK;
+        let subMsg: string = RetMsg.SUC_OK;
+
+        let id = req.query.id; //筛选条件
+     
+
+        console.log('user login params:', id);
+        do {
+            var UserInfoDelInstance = new UserInfoDeleteDao(id);
+            //参数校验
+            if(!UserInfoDelInstance.checkData()) {
+                ret = RetCode.ERR_CLIENT_PARAMS_ERR;
+                msg = RetMsg.ERR_CLIENT_PARAMS_ERR;
+                subMsg = "参数出错";
+                break;
+            }
+            console.log("checkData success!!");
+            
+            //数据库获取数据
+            try {
+                dataRows = await UserInfoDelInstance.doDel()
+            } catch (error) {
+                ret = RetCode.ERR_SERVER_EXCEPTION;
+                msg = RetMsg.ERR_SERVER_EXCEPTION;
+                subMsg = error.message;
+                break;
+            }
+            console.log("getUserFromDB success!!");    
+        } while (false)
+
+        let result = {
+            status: ret,
+            msg   : msg,
+            subMsg: subMsg
         }
         //返回操作
         res.json(result);
